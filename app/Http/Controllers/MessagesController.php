1bf9c17c314e6b1a82c1eae8d77529bb4bc56cee
BKinya <gitongakinya1@gmail.com>
2018-12-06 12:18:26 +0300

    --

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -33,7 +33,7 @@ class MessagesController extends Controller
         
 
         //get tokens
-        $tokens = \App\TokensModel::select('token_string')->get();
+        $tokens = \App\TokensModel::get(['token_string'])->value();
         
         
         
